e686b829db586441c80b4522544cf89c3156fa92
Alex <abraha7@uw.edu>
2024-09-14 17:29:08 +0000

    Added '()' to lines 8-9

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/") 
 def index():
-    cpu_percent = psutil.cpu_percent
-    mem_percent = psutil.virtual_memory.percent
+    cpu_percent = psutil.cpu_percent()
+    mem_percent = psutil.virtual_memory().percent
     Message = None
     if cpu_percent > 80 or mem_percent > 80:
         Message = "High CPU or Memory Utilization detected. Please scale up"
@@ -14,7 +14,7 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    
+
 
 
 
